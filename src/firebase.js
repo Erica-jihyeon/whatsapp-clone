994ee2ebbdb0f1a76eb4adebd1d6c2dbb1eaec24
Erica-jihyeon <jihyeon20@gmail.com>
2022-02-06 00:06:45 -0600

    firebase config bug fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDFNM-MxNQml95MEg8pCP6EuY7g6da3McI",
   authDomain: "whatsapp-clone-46899.firebaseapp.com",
@@ -11,9 +12,12 @@ const firebaseConfig = {
   measurementId: "G-ZQH128H52B"
 };
 
+// Use this to initialize the firebase App
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
 const db = firebaseApp.firestore();
-const auth = firebaseApp.auth();
+const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
 export { auth, provider };
